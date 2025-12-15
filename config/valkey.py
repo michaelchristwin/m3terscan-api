9614ec0038a127287f2da1033cfe08c1a355388f
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-12-15 09:48:06 +0100

    Chore: Updated valkey url

--- config/valkey.py
+++ config/valkey.py
@@ -9,7 +9,7 @@ async def get_client():
     """
     Docstring for get_client
     """
-    addresses = [NodeAddress("redis://red-d4u5ac5actks73csnn9g:6379", 6379)]
+    addresses = [NodeAddress("redis://red-d4u5ac5actks73csnn9g", 6379)]
     config = GlideClientConfiguration(addresses, request_timeout=500)
     client = await GlideClient.create(config)
     return client
