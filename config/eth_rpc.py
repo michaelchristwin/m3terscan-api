3e32c015a3a940dea8b399608bcb702b94175cac
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-07-05 08:39:32 +0100

    chore: Fixed env

--- config/eth_rpc.py
+++ config/eth_rpc.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 from web3 import Web3
 
-load_dotenv(dotenv_path=".env.local")
+load_dotenv(dotenv_path=".env")
 
 API_KEY = os.getenv("INFURA_API_KEY")
 w3 = Web3(Web3.HTTPProvider(f"https://sepolia.infura.io/v3/{API_KEY}"))
